ef5921ef18546158c79b36a4d7497cce8ae3c922
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-07-08 18:22:31 -0400

    commit

--- tasks/extract.py
+++ tasks/extract.py
@@ -2,14 +2,18 @@
 import pandas as pd
 from sodapy import Socrata
 
+# Data url
+url_api = 'data.cityofnewyork.us'
 # App token for the api
 app_token = '3n24PhVfrGN0jpYvSlbkFd7M3'
 
-client = Socrata('data.cityofnewyork.us',
-                 app_token,
-                 username='ekuke003@gmail.com',
-                 password='D2racine4ac#')
+# Function to extract data
+def extract():
+    client = Socrata(url_api,
+                    app_token,
+                    username='ekuke003@gmail.com',
+                    password='D2racine4ac#')
 
-results = client.get('dpec-ucu7', limit=3621)
-results_df = pd.DataFrame.from_records(results)
-print(results_df)
+    results = client.get('dpec-ucu7', limit=3621)
+    results_df = pd.DataFrame.from_records(results)
+    return results_df
