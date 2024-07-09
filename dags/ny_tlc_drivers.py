7a5b3fab65bb7b243f26d16f94f22b6a2568a966
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-07-09 11:18:00 -0400

    commit

--- dags/ny_tlc_drivers.py
+++ dags/ny_tlc_drivers.py
@@ -1,47 +1,53 @@
 # Import libraries
+from tasks import extract, transform, load
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime
 from airflow.operators.bash import BashOperator
-from extract import extract_data
 
 
 
 # Define the Dag
-with DAG("ny_tlc_drivers_app", start_date=datetime(2024, 1, 1),
+with DAG(dag_id="ny_tlc_drivers_app", start_date=datetime(2024, 1, 1),
         schedule_interval="@daily", catchup=False) as dag:
 
         extract_task_01 = PythonOperator(
-                task_id="extract_task_01",
-                python_callable=extract_task,
-                op_args={api_url}
+                task_id="extract_task",
+                python_callable=extract.extract_data
+                # op_args={api_url}
         )
 
         transform_task_01 = PythonOperator(
-                task_id="transform_task_01",
-                python_callable=transform_task,
-                op_args=['{{ ti.xcom_pull(task_ids="extract_task_01") }}']
+                task_id="transform_task",
+                python_callable=transform.transform_data,
+                op_args=['{{ ti.xcom_pull(task_ids="extract_task") }}']
         )
 
         load_task_01 = PythonOperator(
                 task_id="load_task_01",
-                python_callable=load_task,
-                op_args=['{{ ti.xcom_pull(task_ids="transform_task_01") }}']
+                python_callable=load.load_data,
+                op_args=['{{ ti.xcom_pull(task_ids="transform_task") }}']
         )
 
-        load_postgres = PostgresOperator(
-                task_id="create_postgres_table",
+        postgres_task_01 = PostgresOperator(
+                task_id="create_postgre_table",
                 postgres_conn_id="postgres_conn",
                 sql="""
-                        create table if not exists tlc_driver (
+                        create table if not exists tlc_drivers (
                         app_no INT,
                         type VARCHAR,
                         application_date DATE, 
                         status VARCHAR,
                         other_requirements VARCHAR,
                         last_update TIMESTAMP)
+                        primary key app_no
                 """
+        )
+
+        postgres_task_02 = PostgresOperator(
+                task_id="Insert_into_table",
+                
         )
 
         extract_task_01 >> transform_task_01 >> load_task_01
