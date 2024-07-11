b97defba39204d983c25634a4c741c140201aebf
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-07-11 18:14:43 -0400

    commit

--- dags/extract.py
+++ dags/extract.py
@@ -14,14 +14,13 @@ import pandas as pd
 
 def extract_data():
     url = 'https://data.cityofnewyork.us/resource/dpec-ucu7.json'
-    # params = {'$$app_token': '3n24PhVfrGN0jpYvSlbkFd7M3'}
-    response = requests.get(url)
-    data = json.loads(response.content)
+    re = requests.get(url)
+    data = re.json()
     df = pd.json_normalize(data)
-
     return df
 '''
 if __name__ == "__main__":
     extracted_data = extract_data()
 '''
-# print(extract_data())
+extracted_data = extract_data()
+# print(extracted_data)
--- dags/transform.py
+++ dags/transform.py
@@ -3,39 +3,27 @@ from extract import extract_data
 from config import load_config
 import pandas as pd
 import json
+import logging
 
-extracted_data = extract_data()
+logging.BasicConfig(filename='extract.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 # Function to transform the extracted data
-def transform_data(data):
-    # Convert the json_data to a pandas dataframe
-    # df = pd.DataFrame(data)
-    # df = pd.json_normalize(data)
-    # logging.info('Data transformed successfuly')
-
-    # Fix column names
-    df = data.rename(columns={ 
+def transform_data(df):
+    df = df.rename(columns={ 
         'app_date':'application_date', 
         'lastupdate':'last_update'
     })
-    # logging.info('Columns renamed successfully')
-                
     # Drop some uneccessary columns 
     df = df.drop(['fru_interview_scheduled', 
                 'wav_course', 'drug_test', 
                 'defensive_driving', 
                 'driver_exam', 
                 'medical_clearance_form'], axis=1)
-               
-    # Configure pandas to display all rows and columns
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', None)
-    pd.set_option('display.max_colwidth', None)
                 
     return df
 
 # if __name__ == "__main__":
     # config = load_config("/Users/jbshome/Desktop/tlc_drivers_etl/configurations/config.json")
-    # extracted_data = extract_data()
-# print(transform_data(extracted_data)) 
-    # print(transformed_data)
+extracted_data = extract_data()
+transform_data(extracted_data)
+
+print(transform_data)
